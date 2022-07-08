b9810f52c588a6476181d5dd149043ec55c4a6c6
Danielle.D <danielle.djodjoum@gmail.com>
2022-07-08 16:41:37 +0200

    filtre mot

---
+++ back/Services/WordFilterService.cs
@@ -0,0 +1,18 @@
+﻿namespace back.Services
+{
+    public class WordFilterService
+    {
+        //string[] words = System.IO.File.ReadAllLines(@"C:\Users\daniellle.djodjoum\Documents\SCHOOL\M2I\forum\forum-api\back\Ressources");
+        //foreach (string word in words){
+
+        //    string replacement = source.Replace(*);
+
+        //    }
+
+
+
+
+    }
+}
+    
+
